5c045966c0658d7b69f88270c0ffde096fc8deb7
Alexey Katsuro <katsuro.alexey@gmail.com>
2020-12-09 15:07:04 +0300

    fix: update app code

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:pzz/models/app_state.dart';
-import 'package:pzz/routes.dart';
+import 'package:pzz/res/strings.dart';
 import 'package:pzz/theme.dart';
-import 'package:pzz/ui/basket_page.dart';
-import 'package:pzz/ui/home_page.dart';
-import 'package:pzz/ui/not_found_page.dart';
-import 'package:pzz/ui/person_info_page.dart';
-import 'package:pzz/ui/sauces_page.dart';
-import 'package:pzz/utils/widgets/status_bar_brightness.dart';
+import 'package:pzz/ui/containers/main_navigation_container.dart';
 import 'package:redux/redux.dart';
 
 class PzzApp extends StatelessWidget {
@@ -21,12 +16,10 @@ class PzzApp extends StatelessWidget {
     return StoreProvider(
       store: store,
       child: MaterialApp(
-        onGenerateRoute: (settings) => getRoute(context, settings),
-        title: 'Flutter Demo',
-        theme: PzzAppTheme.pzzLightTheme,
-        darkTheme: PzzAppTheme.pzzDarkTheme,
-        initialRoute: Routes.homeScreen,
-      ),
+          title: StringRes.appName,
+          theme: PzzAppTheme.pzzLightTheme,
+          darkTheme: PzzAppTheme.pzzDarkTheme,
+          home: MainNavigationContainer()),
     );
   }
 }
